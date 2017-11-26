a33053b579091b84b5848e426cbd54f8499b40da
Bazhenov Eugen <wtf.czabe@gmail.com>
2017-11-26 19:09:26 +0300

    added fluid bg for all blocks, menu active items, main-info slider

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,11 @@ var browserSync = require('browser-sync').create();
 gulp.task('serve', function() {
 
     browserSync.init({
-        server: "./"
+        open: false,
+        ui: false,
+        server: "./",
+        host: '192.168.1.130',
+        port: 3002,
     });
 
     gulp.watch("./css/*.css").on('change', browserSync.reload);
